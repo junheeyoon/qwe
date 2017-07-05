61d7e0f2b0762dba1cf6796b6095ea3dee128013
junheeyoon <enteryjh@naver.com>
2017-07-05 16:49:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 'use strict';
 
 module.exports = function (requestNumber) {
-  /*  
-    Node.js와 JavaScript 기초를 위한 문제 입니다.
-    module.exports 등은 지금 단계에서는 무시해도 됩니다.
-    FizzBuzz 퀴즈는 다음과 같습니다.
-    - 3의 배수이면 fizz를 리턴한다.
-    - 5의 배수이면 buzz를 리턴한다.
-    - 15의 배수이면 fizzbuzz를 리턴한다.
-    - 이 밖의 숫자는 해당 숫자를 리턴한다.
-    작성한 코드가 정상적인지 확인하려면 test.js 파일을 실행하세요
+  'use strict';
 
-    실행 예
-    ```javascript
-    node test.js
-    ```
-    VSCode를 사용하는 경우 Debug 탭에서 테스트를 실행하세요
-    
-    완료되면 Github에 저장소를 만든 후 본 프로젝트를 업로드 한 후 알려주세요
-        
-    아래에 코드를 작성하세요
-  */  
-  return ''
+module.exports = function (requestNumber) {
+  
+if(requestNumber % 15 == 0) return 'fizzbuzz';
+else if(requestNumber % 5 == 0) return 'buzz';
+else if(requestNumber % 3 == 0) return 'fizz';  
+
+else return requestNumber;
+}
 }
