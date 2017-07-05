65229e5e262340d19451cb41882c66b0ca1278ee
changjoo-park <changjoo.park@partner.so>
2017-07-05 14:08:52 +0900

    FizzBuzz :)

---
+++ test.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const fizzbuzz = require('./app')
+
+// assert는 Node.js 내장 테스트 모듈 입니다.
+// https://nodejs.org/api/assert.html#assert_assert_value_message
+assert.equal(fizzbuzz(0), '0')
+assert.equal(fizzbuzz(2), '2')
+
+assert.equal(fizzbuzz(3), 'fizz')
+assert.equal(fizzbuzz(5), 'buzz')
+assert.equal(fizzbuzz(15), 'fizzbuzz')
+
+assert.equal(fizzbuzz(6), 'fizz')
+assert.equal(fizzbuzz(10), 'buzz')
+assert.equal(fizzbuzz(30), 'fizzbuzz')
