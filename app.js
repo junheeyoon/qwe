facbc0af279c4385217b15341c225b8970de4939
junheeyoon <enteryjh@naver.com>
2017-07-05 16:50:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 'use strict';
 
 module.exports = function (requestNumber) {
-  'use strict';
-
-module.exports = function (requestNumber) {
   
 if(requestNumber % 15 == 0) return 'fizzbuzz';
 else if(requestNumber % 5 == 0) return 'buzz';
@@ -11,4 +8,4 @@ else if(requestNumber % 3 == 0) return 'fizz';
 
 else return requestNumber;
 }
-}
+
